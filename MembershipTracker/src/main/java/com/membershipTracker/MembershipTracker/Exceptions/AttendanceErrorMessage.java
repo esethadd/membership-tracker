a589e1ee63fcbaf3da739a3de6702bec45fc0b16
esethadd <esethadd@gmail.com>
2025-01-04 21:32:40 -0500

    Error Handling

--- MembershipTracker/src/main/java/com/membershipTracker/MembershipTracker/Exceptions/AttendanceErrorMessage.java
+++ MembershipTracker/src/main/java/com/membershipTracker/MembershipTracker/Exceptions/AttendanceErrorMessage.java
@@ -2,10 +2,11 @@ package com.membershipTracker.MembershipTracker.Exceptions;
 
 import lombok.Getter;
 
+
+@Getter
 public enum AttendanceErrorMessage {
     ATTENDANCE_NOT_FOUND("Attendance Record(s) Not Found");
 
-    @Getter
     private final String message;
 
     AttendanceErrorMessage(String message){
--- MembershipTracker/src/main/java/com/membershipTracker/MembershipTracker/Exceptions/MemberErrorMessage.java
+++ MembershipTracker/src/main/java/com/membershipTracker/MembershipTracker/Exceptions/MemberErrorMessage.java
@@ -3,10 +3,10 @@ package com.membershipTracker.MembershipTracker.Exceptions;
 
 import lombok.Getter;
 
+@Getter
 public enum MemberErrorMessage {
     MEMBER_NOT_FOUND("Member Not Found");
 
-    @Getter
     private final String message;
 
     MemberErrorMessage(String message){
